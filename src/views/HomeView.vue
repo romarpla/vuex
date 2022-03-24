09df49fd6ec44f262f52e2f643730d84c4e030c1
romarpla <jrodrigo.martinez@hotmail.com>
2022-03-23 18:33:25 -0600

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Counter/>
+    <ContadorCuadrado/>
+    <Botones/>
+    <ColorCode/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Counter from "../components/Counter.vue";
+import ContadorCuadrado from "../components/ContadorCuadrado.vue";
+import Botones from "../components/Botones.vue";
+import ColorCode from "../components/ColorCode.vue";
 
 export default {
+
   name: 'HomeView',
-  components: {
-    HelloWorld
+  
+  components:{
+    Counter,
+    ContadorCuadrado,
+    Botones,
+    ColorCode
   }
 }
 </script>
+
+<style>
+div{
+  margin-bottom: 10px;
+}
+
+.counter{
+  font-size: 80px;
+}
+
+.buttons button{
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+</style>
